4abfbc56db84fff985ce0df6a967489f2ff567ad
Francisco Ortiz <jose.ortiz.ortiz@est.una.ac.cr>
2024-05-25 21:33:43 -0600

    cambio en icono

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ module.exports = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/book_icon.ico',
   organizationName: 'tu-usuario', 
   projectName: 'mi-backend-docs',
   themeConfig: {
